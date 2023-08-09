18f533a2f2f1f15f8cfe41c436bd8f1d9fc5a5a7
gabrielsalc <gabrielsalcedo.gs@gmail.com>
2023-08-08 23:10:17 -0300

    agregado personal, arreglado links

--- js/MostrarComponentes.js
+++ js/MostrarComponentes.js
@@ -10,7 +10,7 @@
 
 function MostrarComponentes(id){
     var i;
-    for (i = 1; i < 19; i++) {
+    for (i = 1; i < 20; i++) {
         document.getElementById("medico" + i).hidden = true; 
         document.getElementById("seleccionar" + i).removeAttribute("hidden");  
     }
