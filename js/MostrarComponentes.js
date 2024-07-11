b8e494794ad211e7f945e3d76e6fab0a60537c86
Gabriel Salcedo <gabrielsalcedo.gs@gmail.com>
2024-07-10 22:44:11 -0300

    Agregado Julieta Celis

--- js/MostrarComponentes.js
+++ js/MostrarComponentes.js
@@ -10,7 +10,7 @@
 
 function MostrarComponentes(id){
     var i;
-    for (i = 1; i < 24; i++) {
+    for (i = 1; i < 25; i++) {
         document.getElementById("medico" + i).hidden = true; 
         document.getElementById("seleccionar" + i).removeAttribute("hidden");  
     }
